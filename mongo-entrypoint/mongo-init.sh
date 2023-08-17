adafef2652e07a186f058e0f5f4ab110a9e0c6d8
VishalLokam <vishalvikaslokam@gmail.com>
2023-08-17 09:52:33 +0530

    Fixed mongo shell error

--- mongo-entrypoint/mongo-init.sh
+++ mongo-entrypoint/mongo-init.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 echo "Creating new user"
-mongo admin --localhost -u superUser -p superUserPass --eval "db.createUser({user: 'ownerUser', pwd: 'ownerUserPass', roles: [{role: 'dbOwner', db: 'todolistDB'}]});db.createUser({user: 'readWrite', pwd: 'readWritePass', roles: [{role: 'readWrite', db: 'todolistDB'}]});"
+mongosh admin --localhost -u superUser -p superUserPass --eval "db.createUser({user: 'ownerUser', pwd: 'ownerUserPass', roles: [{role: 'dbOwner', db: 'todolistDB'}]});db.createUser({user: 'readWrite', pwd: 'readWritePass', roles: [{role: 'readWrite', db: 'todolistDB'}]});"
 echo "New user created"
