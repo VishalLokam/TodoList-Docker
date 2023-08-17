34ab5fc45acde41a90b2414b539d324320587f9b
VishalLokam <vishalvikaslokam@gmail.com>
2023-08-17 11:41:24 +0530

    Code fix

--- mongo-entrypoint/mongo-init.sh
+++ mongo-entrypoint/mongo-init.sh
@@ -1,4 +1,4 @@
 #! /usr/bin/env bash
 echo "Creating new user"
-mongosh admin --localhost -u superUser -p superUserPass --eval "db.createUser({user: 'ownerUser', pwd: 'ownerUserPass', roles: [{role: 'dbOwner', db: 'todolistDB'}]});db.createUser({user: 'readWrite', pwd: 'readWritePass', roles: [{role: 'readWrite', db: 'todolistDB'}]});"
+mongosh admin --localhost -u superUser -p superUserPass --eval "use todolistDB; db.createUser({user: 'ownerUser', pwd: 'ownerUserPass', roles: [{role: 'dbOwner', db: 'todolistDB'}]});db.createUser({user: 'readWrite', pwd: 'readWritePass', roles: [{role: 'readWrite', db: 'todolistDB'}]});"
 echo "New user created"
