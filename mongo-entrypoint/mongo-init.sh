af91f820950ec62536c0268e0ecc469d9e6e42ba
VishalLokam <vishalvikaslokam@gmail.com>
2023-08-19 10:50:57 +0530

    Fixing docker compose

--- mongo-entrypoint/mongo-init.sh
+++ mongo-entrypoint/mongo-init.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Creating mongo users..."
-mongosh --authenticationDatabase admin --host localhost -u superUser -p superUserPass todolistDB --eval "db.createUser({user: 'ownerUser', pwd: 'ownerUserPass', roles: [{role: 'readWrite', db: 'todolistDB'}]});"
+mongosh --authenticationDatabase admin --host localhost -u superUser -p superUserPass todolistDB --eval "db.createUser({user: 'devUser', pwd: 'devUserPass', roles: [{role: 'readWrite', db: 'todolistDB'}]});"
 echo "Mongo users created."
